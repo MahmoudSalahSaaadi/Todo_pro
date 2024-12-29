bf203e1f65da987de84f90baaa90494ca9368314
Mahmoud Salah Saadi <Mahmoud.Saadi@andalusiagroup.net>
2024-12-29 11:53:24 +0200

    create new project

---
+++ Todo_Website/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+const showModal = ref(false);
+</script>
+<template>
+    <section class="container max-auto py-10 px-10">
+        <button @click="showModal = true"
+            class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Toggle modal
+        </button>
+        <BaseModal :showModal="showModal" @closeModal="showModal = false" />
+    </section>
+</template>
